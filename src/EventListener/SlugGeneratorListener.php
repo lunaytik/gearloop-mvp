0470501470fdcd6fbab490fe32b7bef469cfb1a2
lunaytik <nathandeuil13@gmail.com>
2025-09-24 22:32:50 +0200

    feat(kit): add temp slug on prePersist to avoid error

--- src/EventListener/SlugGeneratorListener.php
+++ src/EventListener/SlugGeneratorListener.php
@@ -3,6 +3,8 @@
 namespace App\EventListener;
 
 use App\Entity\Category;
+use App\Entity\Kit;
+use App\Repository\KitRepository;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
@@ -11,9 +13,10 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Category::class)]
 #[AsEntityListener(event: Events::preUpdate, method: 'preUpdate', entity: Category::class)]
+#[AsEntityListener(event: Events::prePersist, method: 'prePersistKit', entity: Kit::class)]
 final class SlugGeneratorListener
 {
-    public function __construct(private SluggerInterface $slugger)
+    public function __construct(private SluggerInterface $slugger, private KitRepository $kitRepository)
     {
     }
 
@@ -29,5 +32,21 @@ final class SlugGeneratorListener
             $slug = $this->slugger->slug($category->getName())->lower()->toString();
             $category->setSlug($slug);
         }
+    }
+
+    public function prePersistKit(Kit $kit, PrePersistEventArgs $args): void
+    {
+        $slugExist = $this->kitRepository->findOneBy(['slug' => $kit->getSlug()]);
+
+        // TODO:: CHANGE FOR REWRITE WITH 1, 2...
+        if ($slugExist) {
+            $slug = $this->slugger->slug($kit->getName())->lower()->toString();
+            $slug .= '-' . $kit->getId();
+            $kit->setSlug($slug);
+        } else {
+            $slug = $this->slugger->slug($kit->getName())->lower()->toString();
+            $kit->setSlug($slug);
+        }
+
     }
 }
