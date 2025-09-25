80380a715e0f21c38b01f4a651fbb03d08686825
lunaytik <nathandeuil13@gmail.com>
2025-09-25 14:35:27 +0200

    feat(auth): create login/register routes (controllers + templates v0.1) + install daisyui

--- importmap.php
+++ importmap.php
@@ -25,4 +25,11 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'daisyui' => [
+        'version' => '5.1.14',
+    ],
+    'daisyui/daisyui.min.css' => [
+        'version' => '5.1.14',
+        'type' => 'css',
+    ],
 ];
