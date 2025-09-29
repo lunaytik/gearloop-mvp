b1e5574bdaddc5fc8e73613584ce5ddfdc797252
lunaytik <nathandeuil13@gmail.com>
2025-09-29 18:26:00 +0200

    feat(kit): add voter for crud operations to send 404 for non owner or not connected user for private kit

--- src/Controller/KitController.php
+++ src/Controller/KitController.php
@@ -2,41 +2,38 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
-use App\Entity\Item;
-use App\Entity\ItemVariant;
 use App\Entity\Kit;
+use App\Entity\KitItem;
 use App\Form\KitType;
 use App\Repository\KitRepository;
-use App\Service\KitStatsCalculator;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/kit')]
+#[Route('/kits')]
 final class KitController extends AbstractController
 {
-    public function __construct(private KitStatsCalculator $calculator)
-    {
-    }
 
+    #[IsGranted('IS_AUTHENTICATED')]
     #[Route(name: 'app_kit_index', methods: ['GET'])]
     public function index(KitRepository $kitRepository): Response
     {
         return $this->render('kit/index.html.twig', [
-            'kits' => $kitRepository->findAll(),
+            'kits' => $kitRepository->findBy(['owner' => $this->getUser()]),
         ]);
     }
 
+    #[IsGranted('IS_AUTHENTICATED')]
     #[Route('/new', name: 'app_kit_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $kit = new Kit();
+        $kitItem = new KitItem();
+        $kit->addKitItem($kitItem);
+
         $form = $this->createForm(KitType::class, $kit);
         $form->handleRequest($request);
 
@@ -44,12 +41,6 @@ final class KitController extends AbstractController
             $kit->setOwner($this->getUser());
 
             $entityManager->persist($kit);
-
-            if (!$kit->getKitItems()->isEmpty()) {
-                $calculatedStats = $this->calculator->calculateStats($kit);
-                $kit->setStats($calculatedStats);
-            }
-
             $entityManager->flush();
 
             return $this->redirectToRoute('app_kit_index', [], Response::HTTP_SEE_OTHER);
@@ -61,7 +52,8 @@ final class KitController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_kit_show', methods: ['GET'])]
+    #[Route('/{slug:kit}', name: 'app_kit_show', methods: ['GET'])]
+    #[IsGranted('KIT_VIEW', subject: 'kit', message: 'Kit not found', statusCode: Response::HTTP_NOT_FOUND)]
     public function show(Kit $kit): Response
     {
         return $this->render('kit/show.html.twig', [
@@ -70,14 +62,13 @@ final class KitController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_kit_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('KIT_EDIT', subject: 'kit', message: 'Kit not found', statusCode: Response::HTTP_NOT_FOUND)]
     public function edit(Request $request, Kit $kit, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(KitType::class, $kit);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-
             $entityManager->flush();
             return $this->redirectToRoute('app_kit_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -89,6 +80,7 @@ final class KitController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_kit_delete', methods: ['POST'])]
+    #[IsGranted('KIT_DELETE', subject: 'kit', message: 'Kit not found', statusCode: Response::HTTP_NOT_FOUND)]
     public function delete(Request $request, Kit $kit, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$kit->getId(), $request->getPayload()->getString('_token'))) {
