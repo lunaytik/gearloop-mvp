b1c492a40bca8f86192093f1839c7c69c3460db8
lunaytik <nathandeuil13@gmail.com>
2025-10-21 11:57:22 +0200

    feat(explore): add items to explore and use items for user items

--- src/Controller/ExploreController.php
+++ src/Controller/ExploreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ItemRepository;
 use App\Repository\KitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExploreController extends AbstractController
 {
 
-    #[Route('/explore', name: 'app_explore_index')]
-    public function index(KitRepository $kitRepository): Response
+    #[Route('/explore/kits', name: 'app_explore_kits')]
+    public function kits(KitRepository $kitRepository): Response
     {
-        return $this->render('explore/index.html.twig', [
+        return $this->render('explore/kits.html.twig', [
             'totalKits' => $kitRepository->countPublicKits(),
+        ]);
+    }
+
+    #[Route('/explore/items', name: 'app_explore_items')]
+    public function items(ItemRepository $itemRepository): Response
+    {
+        return $this->render('explore/items.html.twig', [
+            'totalItems' => $itemRepository->countActiveItems(),
         ]);
     }
 }
